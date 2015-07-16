28e03156893edcfcb2bf2ca7f2cfff6dc592c885
Sam Hains <sdh@eml.cc>
2015-07-15 21:06:42 -0400

    added bug fix for portal.html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,9 +25,8 @@ class UsersController < ApplicationController
   end
 
   def portal
-    if current_user.discussions
-      @discussions = current_user.discussions.limit(10)
-    end
+    
+    @discussions = current_user.discussions.limit(10)
     recent_discussions_count = 6 - @discussions.count
     if recent_discussions_count > 0
       @recent_discussions = Discussion.order('last_updated DESC').limit(recent_discussions_count)
