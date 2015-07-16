0c53bec0cf130c47a1a7224a398c007a6ba169cb
Sam Hains <sdh@eml.cc>
2015-07-15 21:10:51 -0400

    added to seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.create(name: "sam", username: "sdhains", password: 'lob99lob', bio: "cool dude on the street") 
